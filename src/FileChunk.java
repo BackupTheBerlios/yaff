12a1a77bffc111738acbc6bbe7144b83f2901618
mcbook <devnull@localhost>
2009-09-15 13:01:27 +0200

    increased chunk size to 256 kbyte; added method to truncate files

--- src/FileChunk.java
+++ src/FileChunk.java
@@ -1,14 +1,48 @@
-import java.io.File;
+import java.io.*;
 
 public class FileChunk {
-	public final long CHUNK_SIZE = 64000;
 	
-	private byte[] data;
+	public final long CHUNK_SIZE = 256000;
+	
+	private byte[] data; //chunk data
 	private long index; //index of this particular chunk in a file;
 	
-	public FileChunk(File f, long index){
-		//a bissi file io (chunk einlesen)
+	public FileChunk(File f, long index){		
+		this.index = index;
+		this.data = truncate(f, index);	
 	}
-	
+	/**
+	 * 
+	 * @param file to truncate
+	 * @param index of the chunk 
+	 * @return byte array of the chunk data
+	 */
+	private byte[] truncate(File file, long index){
+		
+		try {
+			RandomAccessFile rf = new RandomAccessFile(file,"r");
+
+			if((rf.length() - index*CHUNK_SIZE) >= CHUNK_SIZE) {
+				data = new byte[(int) CHUNK_SIZE];
+				rf.seek(index * CHUNK_SIZE);
+				rf.read(data);	
+				return data;
+			} else {
+				data = new byte[(int) (rf.length() - index*CHUNK_SIZE)];
+				return data;
+			}
+						
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}		
+	}
+
+	public byte[] getData() {
+		return data;
+	}
+	public long getIndex(){
+		return index;
+	}
 	
 }
--- src/SharedRessource.java
+++ src/SharedRessource.java
@@ -1,11 +1,14 @@
 import java.io.File;
 
-
 public class SharedRessource {
 	private File diskFile;
 	private long chunkCount;
 	private String hash;
 	private boolean complete;
+	
+	public SharedRessource() {
+		
+	}
 	
 	public void write(){}
 
